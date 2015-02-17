ec977f4869991b68852eab18b10430a29b978326
Donovan Young <dyoung522@gmail.com>
2015-02-16 22:34:19 -0500

    add authentication

--- spec/controllers/goals_controller_spec.rb
+++ spec/controllers/goals_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe GoalsController, :type => :controller do
+  login_user
 
   # This should return the minimal set of attributes required to create a valid
   # Goal. As you add validations to Goal, be sure to adjust the attributes here as well.
@@ -22,15 +23,10 @@ RSpec.describe GoalsController, :type => :controller do
     }
   }
 
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # GoalsController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
-
   describe 'GET index' do
     it 'assigns all goals as @goals' do
       goal = Goal.create! valid_attributes
-      get :index, {}, valid_session
+      get :index, {}
       expect(assigns(:goals)).to eq([goal])
     end
   end
@@ -38,14 +34,14 @@ RSpec.describe GoalsController, :type => :controller do
   describe 'GET show' do
     it 'assigns the requested goal as @goal' do
       goal = Goal.create! valid_attributes
-      get :show, {:id => goal.to_param}, valid_session
+      get :show, {:id => goal.to_param}
       expect(assigns(:goal)).to eq(goal)
     end
   end
 
   describe 'GET new' do
     it 'assigns a new goal as @goal' do
-      get :new, {}, valid_session
+      get :new, {}
       expect(assigns(:goal)).to be_a_new(Goal)
     end
   end
@@ -53,7 +49,7 @@ RSpec.describe GoalsController, :type => :controller do
   describe 'GET edit' do
     it 'assigns the requested goal as @goal' do
       goal = Goal.create! valid_attributes
-      get :edit, {:id => goal.to_param}, valid_session
+      get :edit, {:id => goal.to_param}
       expect(assigns(:goal)).to eq(goal)
     end
   end
@@ -62,30 +58,30 @@ RSpec.describe GoalsController, :type => :controller do
     describe 'with valid params' do
       it 'creates a new Goal' do
         expect {
-          post :create, {:goal => valid_attributes}, valid_session
+          post :create, {:goal => valid_attributes}
         }.to change(Goal, :count).by(1)
       end
 
       it 'assigns a newly created goal as @goal' do
-        post :create, {:goal => valid_attributes}, valid_session
+        post :create, {:goal => valid_attributes}
         expect(assigns(:goal)).to be_a(Goal)
         expect(assigns(:goal)).to be_persisted
       end
 
       it 'redirects to the created goal' do
-        post :create, {:goal => valid_attributes}, valid_session
+        post :create, {:goal => valid_attributes}
         expect(response).to redirect_to(Goal.last)
       end
     end
 
     describe 'with invalid params' do
       it 'assigns a newly created but unsaved goal as @goal' do
-        post :create, {:goal => invalid_attributes}, valid_session
+        post :create, {:goal => invalid_attributes}
         expect(assigns(:goal)).to be_a_new(Goal)
       end
 
       it 're-renders the "new" template' do
-        post :create, {:goal => invalid_attributes}, valid_session
+        post :create, {:goal => invalid_attributes}
         expect(response).to render_template('new')
       end
     end
@@ -104,7 +100,7 @@ RSpec.describe GoalsController, :type => :controller do
 
       it 'updates the requested goal' do
         goal = Goal.create! valid_attributes
-        put :update, {:id => goal.to_param, :goal => new_attributes}, valid_session
+        put :update, {:id => goal.to_param, :goal => new_attributes}
         goal.reload
         expect(goal.topic).to eq new_attributes[:topic]
         expect(goal.date_due).to eq new_attributes[:date_due]
@@ -113,13 +109,13 @@ RSpec.describe GoalsController, :type => :controller do
 
       it 'assigns the requested goal as @goal' do
         goal = Goal.create! valid_attributes
-        put :update, {:id => goal.to_param, :goal => valid_attributes}, valid_session
+        put :update, {:id => goal.to_param, :goal => valid_attributes}
         expect(assigns(:goal)).to eq(goal)
       end
 
       it 'redirects to the goal' do
         goal = Goal.create! valid_attributes
-        put :update, {:id => goal.to_param, :goal => valid_attributes}, valid_session
+        put :update, {:id => goal.to_param, :goal => valid_attributes}
         expect(response).to redirect_to(goal)
       end
     end
@@ -127,13 +123,13 @@ RSpec.describe GoalsController, :type => :controller do
     describe 'with invalid params' do
       it 'assigns the goal as @goal' do
         goal = Goal.create! valid_attributes
-        put :update, {:id => goal.to_param, :goal => invalid_attributes}, valid_session
+        put :update, {:id => goal.to_param, :goal => invalid_attributes}
         expect(assigns(:goal)).to eq(goal)
       end
 
       it 're-renders the "edit" template' do
         goal = Goal.create! valid_attributes
-        put :update, {:id => goal.to_param, :goal => invalid_attributes}, valid_session
+        put :update, {:id => goal.to_param, :goal => invalid_attributes}
         expect(response).to render_template('edit')
       end
     end
@@ -143,13 +139,13 @@ RSpec.describe GoalsController, :type => :controller do
     it 'destroys the requested goal' do
       goal = Goal.create! valid_attributes
       expect {
-        delete :destroy, {:id => goal.to_param}, valid_session
+        delete :destroy, {:id => goal.to_param}
       }.to change(Goal, :count).by(-1)
     end
 
     it 'redirects to the goals list' do
       goal = Goal.create! valid_attributes
-      delete :destroy, {:id => goal.to_param}, valid_session
+      delete :destroy, {:id => goal.to_param}
       expect(response).to redirect_to(goals_url)
     end
   end
