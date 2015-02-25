55ce15971acaa70ca487522e941474722874a6a5
Donovan Young <dyoung522@gmail.com>
2015-02-24 20:11:06 -0500

    Create admin user if one doesn't exist

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+defaults = {
+    name: 'Admin',
+    email: 'admin@example.com',
+    password: 'admin123'
+}
+
+if User.count == 0
+  puts 'Creating Admin user with defaults: ' +
+    "email = '#{defaults[:email]}', password = '#{defaults[:password]}'"
+  User.create! defaults
+end
